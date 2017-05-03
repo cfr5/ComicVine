7f91a25d04792ab3591a1f9e63eeadc4b2424f31
Chusca <jesus.maneiro.figueroa@gmail.com>
2017-05-03 13:44:31 +0200

    Implementado login con google

--- pint/comics/urls.py
+++ pint/comics/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from comics import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
--- pint/pint/settings.py
+++ pint/pint/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'comics',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'pint.urls'
@@ -63,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -70,6 +74,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pint.wsgi.application'
 
+# Backends the autenticacion de Google+ y Facebook
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'social_core.backends.google.GoogleOAuth2',
+)
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'index'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1035917129362-39g730g3q33ouc9hna8i0p5r1m80n201.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'qUoU-bjsnWRjhAYUoNn25ocl'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
