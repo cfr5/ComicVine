7844c7f7710317bde66ea8dc2809d39ddbe7a150
Carlos Franco Romero <c.franco@udc.es>
2017-05-04 20:15:56 +0200

    añadindo email al registro

--- pint/comics/views.py
+++ pint/comics/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from comics.forms import UserCreationForm
 # Create your views here.
 def index(request):
     return render(request, 'comics/index.html')
@@ -16,8 +17,9 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(username=username, email=email, password=raw_password)
             login(request, user)
             return redirect('index')
     else:
