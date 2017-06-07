87bdcb6539ed84c5b835d11712594d4a35208bba
Carlos Franco Romero <c.franco@udc.es>
2017-06-07 18:32:47 +0200

    recreada base de datos

--- pint/comics/models.py
+++ pint/comics/models.py
@@ -6,10 +6,15 @@ from django.db import models
 
 class Character (models.Model):
     character_id = models.CharField(max_length=50,primary_key = 'true')
-    name = models.CharField(max_length=100)
+    super_name = models.CharField(max_length=100)
     real_name = models.CharField(max_length=100)
+    aliases = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    character_type = models.CharField(max_length=100)
     powers = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
+    origin = models.CharField(max_length=10000)
     def __unicode__(self):
         return self.character_id
 
@@ -19,6 +24,8 @@ class Comic (models.Model):
     title = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
     characters = models.ManyToManyField(Character, verbose_name="list of characters")
+    store_date = models.CharField(max_length=100)
+    synopsis = models.CharField(max_length=10000)
     def __unicode__(self):
         return self.comic_id
 
@@ -26,6 +33,12 @@ class Author (models.Model):
     author_id = models.CharField(max_length=50,primary_key = 'true')
     name = models.CharField(max_length=100)
     comics = models.ManyToManyField(Comic, verbose_name="list of comics")
+    town = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100)
+    birth_date = models.CharField(max_length=100)
+    biography = models.CharField(max_length=10000)
     def __unicode__(self):
         return self.author_id
 
