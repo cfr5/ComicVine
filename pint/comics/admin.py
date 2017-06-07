8a2b6cdc5918397e9a7c897fb2769adbc275db69
Chusca <jesus.maneiro.figueroa@gmail.com>
2017-06-07 15:55:12 +0200

    añadidos modelos al panel admin

--- pint/comics/admin.py
+++ pint/comics/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from comics.models import Character, Comic, Author, UserFollows
 # Register your models here.
+
+admin.site.register(Character)
+admin.site.register(Comic)
+admin.site.register(Author)
+admin.site.register(UserFollows)
