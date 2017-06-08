81c5e5bbbd0ed63ceb674fdff7712383e10038e7
Carlos Franco Romero <c.franco@udc.es>
2017-06-08 15:19:07 +0200

    modificando base de datos

--- pint/comics/admin.py
+++ pint/comics/admin.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from comics.models import Character, Comic, Author, UserFollows
+from comics.models import Character, Comic, Author, ComicFollows, AuthorFollows, CharacterFollows
 # Register your models here.
 
 admin.site.register(Character)
 admin.site.register(Comic)
 admin.site.register(Author)
-admin.site.register(UserFollows)
+admin.site.register(ComicFollows)
+admin.site.register(AuthorFollows)
+admin.site.register(CharacterFollows)
--- pint/comics/models.py
+++ pint/comics/models.py
@@ -6,14 +6,14 @@ from django.db import models
 
 class Character (models.Model):
     character_id = models.CharField(max_length=50,primary_key = 'true')
-    super_name = models.CharField(max_length=100)
-    real_name = models.CharField(max_length=100)
-    aliases = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
-    character_type = models.CharField(max_length=100)
-    powers = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
+    super_name = models.CharField(max_length=1000)
+    real_name = models.CharField(max_length=1000)
+    aliases = models.CharField(max_length=1000)
+    publisher = models.CharField(max_length=1000)
+    gender = models.CharField(max_length=1000)
+    character_type = models.CharField(max_length=1000)
+    powers = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
     origin = models.CharField(max_length=10000)
     def __unicode__(self):
         return self.character_id
@@ -21,29 +21,50 @@ class Character (models.Model):
 class Comic (models.Model):
     comic_id = models.CharField(max_length=50,primary_key = 'true')
     issue_number = models.IntegerField()
-    title = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
+    title = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
     characters = models.ManyToManyField(Character, verbose_name="list of characters")
-    store_date = models.CharField(max_length=100)
+    store_date = models.CharField(max_length=1000)
     synopsis = models.CharField(max_length=10000)
     def __unicode__(self):
         return self.comic_id
 
 class Author (models.Model):
     author_id = models.CharField(max_length=50,primary_key = 'true')
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
     comics = models.ManyToManyField(Comic, verbose_name="list of comics")
-    town = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
-    alias = models.CharField(max_length=100)
-    birth_date = models.CharField(max_length=100)
+    town = models.CharField(max_length=1000)
+    country = models.CharField(max_length=1000)
+    gender = models.CharField(max_length=1000)
+    alias = models.CharField(max_length=1000)
+    birth_date = models.CharField(max_length=1000)
     biography = models.CharField(max_length=10000)
+    image = models.CharField(max_length=1000)
+
     def __unicode__(self):
         return self.author_id
-
+'''
 class UserFollows (models.Model):
     user_id = models.CharField(max_length=50,primary_key = 'true')
     characters = models.ManyToManyField(Character, verbose_name="list of characters")
     comics = models.ManyToManyField(Comic, verbose_name="list of comics")
     authors = models.ManyToManyField(Author, verbose_name="list of authors")
+'''
+
+class ComicFollows(models.Model):
+    follows_id = models.AutoField(primary_key=True)
+    comic = models.OneToOneField(Comic)
+    user_id = models.CharField(max_length=50)
+    follows = models.DateTimeField(auto_now=True)
+
+class AuthorFollows(models.Model):
+    follows_id = models.AutoField(primary_key=True)
+    author = models.OneToOneField(Author)
+    user_id = models.CharField(max_length=50)
+    follows = models.DateTimeField(auto_now=True)
+
+class CharacterFollows(models.Model):
+    follows_id = models.AutoField(primary_key=True)
+    character = models.OneToOneField(Character)
+    user_id = models.CharField(max_length=50)
+    follows = models.DateTimeField(auto_now=True)
